35113d58cfe2192b8719f2cf7db2e36d1e760b21
jislam <engr.jislam@gmail.com>
2018-06-04 13:25:43 +0300

    refactoring done!

--- src/main/java/com/maxpro/support/adapter/FacebookSignInAdapter.java
+++ src/main/java/com/maxpro/support/adapter/FacebookSignInAdapter.java
@@ -1,4 +1,4 @@
-package com.maxpro.support.model;
+package com.maxpro.support.adapter;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
